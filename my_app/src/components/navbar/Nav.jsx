604a6b41f3255ad7f9672cefef806056f1c2d32d
Muhammad Anss <ansslucky07@gmail.com>
2025-10-13 23:45:48 +0300

    z

---
+++ my_app/src/components/navbar/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import pic from './as'
+const Nav = () => {
+  return (
+    <div>
+        <h1>Hello Guys</h1>
+        <div><img src={pic} alt="" /></div>
+    </div>
+  )
+}
+
+export default Nav
